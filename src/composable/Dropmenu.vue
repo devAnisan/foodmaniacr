01f65b6c71adbe6d6446f046e6743ae1dabb1a18
devAnisan <animan667@gmail.com>
2026-04-25 23:45:43 -0600

    fix

--- src/composable/Dropmenu.vue
+++ src/composable/Dropmenu.vue
@@ -2,12 +2,15 @@
   <ul
     class="flex flex-col p-2 box-border absolute top-25 right-4 bg-white rounded-lg shadow-lg w-40 space-y-2"
   >
-    <li class="p-1"><a href="#">Home</a></li>
-    <li class="p-1"><a href="#">Menú</a></li>
-    <li class="p-1"><a href="#">¿Dónde comprar?</a></li>
-    <li class="p-1"><a href="#">Contacto</a></li>
-    <button class="border p-2 pl-3 pr-3 rounded-full hover:cursor-pointer">
-      Ordena aquí
+    <li class="p-1"><a href="#">Inicio</a></li>
+    <li class="p-1"><a href="#menu">Menú</a></li>
+    <li class="p-1"><a href="#sucursales">Sucursales</a></li>
+    <li class="p-1"><a href="#dondeComprar">¿Dónde comprar?</a></li>
+    <button
+      id="extrabold"
+      class="border my-0.5 p-2 pl-3 pr-3 rounded-full hover:cursor-pointer"
+    >
+      <a href="/menu" target="_blank">Ordena aquí</a>
     </button>
   </ul>
 </template>
